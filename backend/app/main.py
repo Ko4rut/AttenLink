6e19407be8b46b8187f8eeefe5f442273e7ada72
NguyenToanThang-kotaru <thangkun07@gmail.com>
2026-04-09 10:44:59 +0700

    Feat LAN

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,8 +7,8 @@ app = FastAPI()
 origins = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "http://192.168.100.108:3000",
 ]
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ['192.168.31.63'],
+  allowedDevOrigins: ['192.168.100.108'],
 };
 
 export default nextConfig;  
