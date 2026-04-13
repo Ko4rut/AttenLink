89793621ce710f89531964e0170624cb60555c3f
NguyenToanThang-kotaru <thangkun07@gmail.com>
2026-04-13 16:55:31 +0700

    Fix: Axios client

--- frontend/lib/apiClient.ts
+++ frontend/lib/apiClient.ts
@@ -1,16 +1,24 @@
-// lib/apiClient.ts
 import axios from "axios";
 
 export const apiClient = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
   timeout: 10000,
+  headers: {
+    "Content-Type": "application/json",
+    "ngrok-skip-browser-warning": "any",
+  },
 });
 
 apiClient.interceptors.request.use((config) => {
   const token = localStorage.getItem("access_token");
+
+  config.headers = config.headers ?? {};
+  config.headers["ngrok-skip-browser-warning"] = "any";
+
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
+
   return config;
 });
 
